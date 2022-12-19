3c1b8cecbb0c2df9a6bd9d40dd679ad0396eb923
void-spark <81029971+void-spark@users.noreply.github.com>
2022-12-19 22:08:00 +0100

    Move some things to util

---
+++ work/util.js
@@ -0,0 +1,42 @@
+exports.forSquare = (xCenter, yCenter, steps, spacing, callback) => {
+    const increase = spacing + 1;
+    const outer = steps * increase;
+
+    for (let x = -outer; x <= outer; x += increase) {
+        const xOnEdge = x == -outer || x == outer;
+        for (let y = -outer; y <= outer; y += increase) {
+            const yOnEdge = y == -outer || y == outer;
+            if (xOnEdge || yOnEdge) {
+                if (callback(xCenter + x, yCenter + y)) {
+                    return;
+                }
+            }
+        }
+    }
+}
+
+exports.canBuild = (room, x, y) => {
+    const look = room.lookAt(x, y);
+    var ok = true;
+    look.forEach(obj => {
+        if (obj.type == LOOK_TERRAIN && obj[LOOK_TERRAIN] == 'wall') {
+            ok = false;
+        } else if (obj.type == LOOK_STRUCTURES) {
+            ok = false;
+        }
+    });
+    return ok;
+}
+
+exports.notBlocked = (room, x, y) => {
+    const look = room.lookAt(x, y);
+    var ok = true;
+    look.forEach(obj => {
+        if (obj.type == LOOK_TERRAIN && obj[LOOK_TERRAIN] == 'wall') {
+            ok = false;
+        } else if (obj.type == LOOK_STRUCTURES) {
+            ok = false;
+        }
+    });
+    return ok;
+}
